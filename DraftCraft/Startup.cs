c51e6f45911f6504e9abc52449baff2158752db2
Szabo Erik <szabove@gmail.com>
2018-03-28 19:44:56 +0200

    Add project files.

---
+++ DraftCraft/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DraftCraft.Startup))]
+namespace DraftCraft
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
