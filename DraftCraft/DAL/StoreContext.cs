f0a28bb1f72feac8b4c6d7ed8fbce303677f8405
Szabo Erik <szabove@gmail.com>
2018-03-29 16:12:39 +0200

    Single Commit

---
+++ DraftCraft/DAL/StoreContext.cs
@@ -0,0 +1,11 @@
+﻿using DraftCraft.Models;
+using System.Data.Entity;
+
+namespace DraftCraft.DAL
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Proizvod> Proizvodi { get; set; }
+        public DbSet<Kategorija> Kategorije { get; set; }
+    }
+}
